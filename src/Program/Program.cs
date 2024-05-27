f99ad9b1d89d15984b9d7c30650557322fdc7088
enzobueno11 <127800256+enzobueno11@users.noreply.github.com>
2024-05-27 20:30:10 -0300

    parte de parte 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,12 +2,37 @@
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
 
+
 namespace CompAndDel
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
+            Parte1();
+            
+        }
+
+        /// <summary>
+        /// Parte 1 
+        /// </summary>
+        public static void Parte1()
+        {
+
+
+            PipeNull pipeNull = new PipeNull();
+
+            PipeSerial pipeSerial1 = new PipeSerial(new FilterNegative(), pipeNull);
+
+            PipeSerial pipeSerial2 = new PipeSerial(new FilterGreyscale(), pipeSerial1);
+
+            PictureProvider provider = new PictureProvider();
+
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            IPicture modifiedImage = pipeSerial2.Send(picture);
+
+            provider.SavePicture(modifiedImage, @"modifiedBeer1.jpg");
         }
     }
 }
